323bf4855a182ff612bbba9bc350ffe5d079e35d
Mati <peraltamati1991@gmail.com>
2021-10-26 22:38:41 -0300

    Se agrega el proyecto

---
+++ desafio.js
@@ -0,0 +1,20 @@
+
+
+var nombre= prompt("Hola, Como te llamas");
+var edad= prompt("¿que edad tenes?");
+
+alert("Hola "+nombre+". Tenes: "+edad+"años");
+
+alert("Vamos a calcular el area de un triangulo te pedire los siguientes datos...");
+
+var base= prompt("Introduce la Base del triagulo: ");
+var altura= prompt("Introduce la altura del triangulo");
+
+var area;
+
+area=(base*altura)/2;
+
+alert("El area del triangulo es: "+area+" cm²");
+
+
+
